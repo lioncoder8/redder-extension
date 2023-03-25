a43d842c9e31dcd684c51095c26d35e1bc26f71c
lioncoder8 <lioncoder8@gmail.com>
2023-03-25 18:34:00 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,13 @@
+function reddenPage() {
+    document.body.style.backgroundColor = 'lightgreen';
+  }
+  
+  chrome.action.onClicked.addListener((tab) => {
+    if (!tab.url.includes('chrome://')) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: reddenPage
+      });
+    }
+  });
+  
